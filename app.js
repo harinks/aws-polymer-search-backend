8ff5e2fbba02f3afeed05157d3ec0b811e9e7031
nkshariharan <nkshariharan17@gmail.com>
2022-03-01 11:22:46 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
+
+
+app.listen(PORT, () => console.log(`Server is running on port-${PORT}`));
