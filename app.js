1cdf2207b9c080755d175cc07b7ba447fb25e135
nkshariharan <nkshariharan17@gmail.com>
2022-03-01 13:14:27 +0530

    repo route

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
+const cors = require("cors");
+const repoRoute = require("./Routes/repo");
+const tagsRoute = require('./Routes/tags')
+
+app.use(cors())
+app.use(express.json())
 
 const dbConnect = require('./config/dbConnect');
 dbConnect()
+
+app.use("/repo",repoRoute)
+app.use("/tags",tagsRoute)
 
 app.listen(PORT, () => console.log(`Server is running on port-${PORT}`));
