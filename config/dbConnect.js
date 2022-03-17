f36250098d1712c47e22c594f7ff63c460586bab
nkshariharan <nkshariharan17@gmail.com>
2022-03-17 18:07:58 +0530

    dbconfig

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,10 +1,18 @@
-const mongoose = require('mongoose');
+const { MongoClient } = require("mongodb")
 
-const connect = () => {
-    mongoose.connect(process.env.URL, {}, (err) =>{
-        if(err) throw err;
-        console.log('database connected');
-    })
+const client= new MongoClient(process.env.URL)
+
+module.exports={
+    db:null,
+
+    repo:null,
+ 
+   async connect()
+   {
+       await client.connect()
+       this.db=client.db(process.env.NAME)
+       console.log("Connected to DB -",process.env.NAME)
+       this.repo=this.db.collection("repo")
+   }
+    
 }
-
-module.exports = connect;
